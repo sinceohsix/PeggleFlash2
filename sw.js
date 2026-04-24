3c8c7714f7f73de718d38a8ce4c118493eaaf454
eli 💫 <44753505+sinceohsix@users.noreply.github.com>
2026-04-23 17:30:33 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,14 @@
 const overrides = {};
 
-self.addEventListener(‘message’, e => {
-if (e.data.type === ‘OVERRIDE’) {
-overrides[e.data.filename] = e.data.buffer;
-}
-if (e.data.type === ‘CLEAR’) {
-for (const k in overrides) delete overrides[k];
-}
+self.addEventListener('message', e => {
+  if (e.data.type === 'OVERRIDE') {
+    overrides[e.data.filename] = e.data.buffer;
+  }
 });
 
-self.addEventListener(‘fetch’, e => {
-const filename = e.request.url.split(’/’).pop().split(’?’)[0];
-if (overrides[filename]) {
-e.respondWith(new Response(overrides[filename]));
-}
+self.addEventListener('fetch', e => {
+  const filename = e.request.url.split('/').pop().split('?')[0];
+  if (overrides[filename]) {
+    e.respondWith(new Response(overrides[filename]));
+  }
 });
